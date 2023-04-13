c13339d34f08a773da4e36439df240b228127b7f
KevinMathew24 <kevinmathewmanoj@gmail.com>
2023-04-13 22:44:30 +0530

    initial commit

---
+++ dart1/bin/dart1.dart
@@ -0,0 +1,126 @@
+import 'dart:io'; //import for taking input and output
+
+void main() {
+  //var a = stdin.readLineSync(); //input syntax
+  //stdout.write(a); // output syntax
+  //print('\n $a Ballz!');
+
+  //data tyoes and var familiarization
+  //int aa = 1;
+  // print('aa=$aa');
+
+  // double b = 0;
+  // print('b=$b');
+  // num c = 0;
+  // print('c=$c');
+  // var d;
+  // d = aa + b;
+  // print('sum=$d');
+  // print('sum=${aa + b}');
+
+  //string familiarization
+  // String name = "Kevin";
+  // print('Hello $name');
+  // String addr = '''Amal jyothi college of engg
+  // kanjirapally,
+  // kottayam ''';
+  // print(addr);
+  // print(addr.length);
+
+  //control statements
+  //if statements
+  // var year = 2013;
+  // if (year >= 2001) {
+  //   print('21st century');
+  // } else if (year < 2001) {
+  //   print('20th century');
+  // } else {
+  //   print('invalid year');
+  // }
+
+  //for loops
+  // for (var i = 0; i < 10; i++) {
+  //   print(i);
+  // }
+
+  //while loop
+  // var i = 0;
+  // while (i < 10) {
+  //   print(i);
+  //   i++;
+  // }
+
+  // simple calculator
+  // print('enter a: ');
+  // num a = double.parse(stdin.readLineSync()!);
+  // print('enter b: ');
+  // num b = double.parse(stdin.readLineSync()!);
+  // print(''' A is: $a
+  // B is $b''');
+  // num sum = a + b;
+  // num diff = a - b;
+  // num prod = a * b;
+  // num frac = a / b;
+  // print('''Operations performed:
+  // 1. Addition: $sum
+  // 2. Subtraction: $diff
+  // 3. Multiplication: $prod;
+  // 4. Division: $frac''');
+
+  //list familiarization
+  // List<int> numblist = [1, 2, 3, 4, 5];
+  // print(numblist);
+  // for (int i in numblist) {
+  //   print(i);
+  // }
+
+  // List<String> numblist1 = ['1', '2', '3', '4', '5'];
+  // print(numblist1);
+  // for (String j in numblist1) {
+  //   print(j);
+  // }
+
+  // var ligma = [1, 2, 3, 4, 5];
+  // for (int i in ligma) {
+  //   print(i);
+  // }
+  // ligma.add(10);
+  // ligma.removeAt(0);
+  // ligma.remove(2);
+  // print(ligma);
+
+  //nested list
+  // List<List<dynamic>> yeet = [
+  //   ['1', '2', '3'],
+  //   [1, 2, 3]
+  // ];
+  // print(yeet);
+  // print(yeet[1][2]);
+  // for (List k in yeet) {
+  //   for (dynamic l in k) {
+  //     print(l);
+  //   }
+  // }
+
+  // //map
+  // Map<String, int> map = {'Ram': 90, 'Arjun': 50, 'Riya': 75};
+  // print(map);
+  // print(map['Ram']);
+
+  // //set {it has no duplicate elements}
+  // Set<int> set1 = {1, 2, 3, 4,4,4,4, 5, 6};
+  // print(set1);
+
+  // function
+  //fibonoacci
+  // var result = fibonacci(20);
+  // print(result);
+}
+//function
+
+//fibonoacci
+// int fibonacci(int n) {
+//   if(n == 0|| n == 1) return n;
+//   return fibonacci(n-1) + fibonacci(n-2);
+// }
+
---
+++ flfile/android/app/src/main/kotlin/com/example/flfile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flfile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
